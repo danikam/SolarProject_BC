324dc98020dfdb6aacc53aee0d04b99672cd9e77
Danika MacDonell <danikam1@uvic.ca>
2019-03-11 15:43:42 -0700

    Updated DownloadLoadFiles.sh to handle the slightly different naming and file formats for 2007 and 2008.

--- bash/DownloadLoadFiles.sh
+++ bash/DownloadLoadFiles.sh
@@ -17,14 +17,23 @@ do
   rm BalancingAuthorityLoad${year}.xls BalancingAuthorityLoad${year}.csv
 
   # Download the file
-  wget https://www.bchydro.com/content/dam/BCHydro/customer-portal/documents/corporate/suppliers/transmission-system/balancing_authority_load_data/Historical%20Transmission%20Data/BalancingAuthorityLoad${year}.xls
+  if [ $year -lt 2007 ]; then
+    wget https://www.bchydro.com/content/dam/BCHydro/customer-portal/documents/corporate/suppliers/transmission-system/balancing_authority_load_data/Historical%20Transmission%20Data/BalancingAuthorityLoad${year}.xls
+  else
+    wget https://www.bchydro.com/content/dam/BCHydro/customer-portal/documents/corporate/suppliers/transmission-system/balancing_authority_load_data/Historical%20Transmission%20Data/${year}controlareaload.xls
+    mv ${year}controlareaload.xls BalancingAuthorityLoad${year}.xls
+  fi
 
   # Convert it to csv format
   ssconvert BalancingAuthorityLoad${year}.xls BalancingAuthorityLoad${year}.csv
 
   # Copy all but the first two lines to a new text file
-  tail -n+3 BalancingAuthorityLoad${year}.csv > temp && mv temp BalancingAuthorityLoad${year}.csv
+  if [ $year -lt 2007 ]; then
+    tail -n+3 BalancingAuthorityLoad${year}.csv > temp && mv temp BalancingAuthorityLoad${year}.csv
+  else
+    tail -n+4 BalancingAuthorityLoad${year}.csv > temp && mv temp BalancingAuthorityLoad${year}.csv
+  fi
 
   # Append a new header that will be readable by pyspark
-  echo '# Date, Hour, Load (GWh)' | cat - BalancingAuthorityLoad${year}.csv > temp && mv temp BalancingAuthorityLoad${year}.csv
+  echo '# Date, Hour, Load (MWh)' | cat - BalancingAuthorityLoad${year}.csv > temp && mv temp BalancingAuthorityLoad${year}.csv
 done
