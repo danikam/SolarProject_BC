994cc5cd0fe2af9c4c637ed4f991d19057ff0b50
Danika MacDonell <danikam1@uvic.ca>
2019-03-13 02:04:12 +0000

    Updated MapIntegratedIrradiances.py to create a DF out of an RDD, with rows indicating the coordinates, date, and GHI. Saved the DF to HDFS, and also saved a DF with the average GHI per set of coordinates to HDFS.

--- python/MapIntegratedIrradiances.py
+++ python/MapIntegratedIrradiances.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+# Date:     180312                                                                                                                                                                                                                            
+# Purpose:   Calculate the integrated solar irradiance for all locations in BC over all years from 2002 to 2008, and visualize as a 2D colour plot. Normalize the integrated irradiances to make a probability distribution for locating the solar farms.  
+
 import findspark
 findspark.init()
 from pyspark import SparkContext
 from pyspark import SparkConf
 from pyspark.sql import SQLContext
+from pyspark.sql import Window
+from pyspark.sql import functions as F
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -11,38 +16,63 @@ from shutil import copyfile
 import os
 import io
 import glob
+#from hdfs import InsecureClient
+import subprocess
+import datetime as dt
+from pytz import timezone
 
 sc=SparkContext()
 sql=SQLContext(sc)
 
+start_time = time.time()
+
 # Get the path to the top level of the repo
 with open(".PWD") as f:
   repo_path = f.readline().strip()
-
-# Function to make a triplet (lat, long, line) for each line in a given file
+  
+# Function to make an ntuple (lat, long, year, time stamp, GHI) for each line in a given file
 # The latitude and longitude are parsed from the filename f
 def proc(f):
-  return sc.textFile(f).map(lambda x: (float((f.split("/")[-1])[0:5]), float((f.split("/")[-1])[6:13]), x))
-
-# Create an RDD from the union of triplets obtained from each irradiance file
-irr_RDD = sc.union([proc(f) for f in glob.glob("%s/Tables/IrradianceData_isInBC/*.csv"%repo_path)])
-
-# Convert each time in a line to a year and timestamp
-
-print(irr_RDD.take(1))
-
-# Read in the irradiance files as whole text files, and pair them with their coordinates
-#flnms_RDD=sc.textFile('file:///Users/danikamacdonell/Courses/Phys511A/SolarProject/Tables/IrradianceData_isInBC/list.txt')
-
-#coords_RDD=flnms_RDD.map(lambda x: (x.split("_")[0], (x.split("_")[1])[0:7]))
   
-#irr_RDD = sc.textFile("/Users/danikamacdonell/Courses/Phys511A/SolarProject/Tables/IrradianceData_isInBC/*.csv")
-#print(irr_RDD).inputFiles()
-  #irr_RDD = sc.wholeTextFiles("/Users/danikamacdonell/Courses/Phys511A/SolarProject/Tables/IrradianceData_isInBC/*.csv").\
-#  map(lambda x: np.genfromtxt(io.StringIO(x[1]), delimiter=",")[:,[0,2,3,4,5]])
+  # Function to make the data line for the given file f
+  def MakeDataLine(line):
+    line_list = line.split(',')
+    year = int(line_list[0])
+    month = int(line_list[2])
+    day = int(line_list[3])
+    hour = int(line_list[4])
+    GHI = int(line_list[5])
+    
+    # Convert the year, month, day, and hour (in UTC) to a timestamp
+    time_dt = dt.datetime(year, month, day, hour)
+    time_utc = timezone('UTC').localize(time_dt)
+    timestamp = time_utc.timestamp()
+    return (float((f.split("/")[-1])[0:5]), float((f.split("/")[-1])[6:13]), year, timestamp, GHI)
 
-#paired_RDD = coords_RDD.zip(irr_RDD)
+  return sc.textFile(f).map(MakeDataLine)
 
+# Get a list of all files in the IrradianceData_isInBC directory in HDFS
+p=subprocess.Popen(["/opt/software/hadoop-2.8.5/bin/hadoop", "fs", "-ls", "-C", "/user/ubuntu/IrradianceData_isInBC/*.csv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = p.communicate()
+filelist = out.decode("utf-8").split('\n')[:-1]
 
+# Create an RDD from the union of ordered ntuples obtained from each irradiance file
+#irr_RDD = sc.union([proc(f) for f in glob.glob("file://%s/Tables/IrradianceData_isInBC/*.csv"%repo_path)])
+irr_RDD = sc.union([proc(f) for f in filelist])  
+#print(irr_RDD.take(1))
 
-              #print(paired_RDD.take(1))
+# Convert the RDD to a dataframe
+irr_DF = irr_RDD.toDF(["Lat", "Long", "Year", "Timestamp", "GHI"])
+#irr_DF.show(n=2)
+
+# Save the full irradiance dataframe to HDFS
+irr_DF.write.save('/user/ubuntu/IrradianceMap/TimeSeriesIrradiance_df', format='csv', mode='overwrite')
+
+# Obtain the average irradiance for each latitude and longitude
+irr_avg_DF = irr_DF.groupby("Lat", "Long").avg("GHI")
+#irr_avg_DF.show(n=10)
+
+# Save the average irradiance dataframe to HDFS
+irr_avg_DF.write.save('/user/ubuntu/IrradianceMap/AverageIrradiance_df', format='csv', mode='overwrite')
+
+print("Time elapsed: %ds"%(time.time()-start_time))
