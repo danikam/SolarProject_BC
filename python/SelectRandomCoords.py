bf8da7edc8fa94d557bc02c67b91c4e1e7f8a680
Danika MacDonell <danikam1@uvic.ca>
2019-03-19 02:29:38 +0000

    Updated SelectRandomCoords.py to randomly select sites from a Gaussian distribution peaked at the highest-GHI site.

--- python/SelectRandomCoords.py
+++ python/SelectRandomCoords.py
@@ -9,30 +9,46 @@ from pyspark.sql.types import FloatType
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import time
+import pandas as pd
 import geopandas as gpd
 import numpy as np
+from pyspark.sql.functions import lit
 
 sc=SparkContext()
 sql=SQLContext(sc)
 
 start_time = time.time()
 
-# Read in the dataframe of average GHI per set of coordinates
+# Function to randomly select 100 indices from a Gaussian distribution of indices peaked at the maximum index (N_coords-1)
+def sample_gaussian(nSamples, N_coords):
+    samples = np.random.normal(1,0.1, N_coords*3)
+    indices_selected = np.unique(np.floor(samples[(samples<1)&(samples>0)]*N_coords))
+    indices = np.random.choice(indices_selected, nSamples, replace=False)
+    return indices.astype(int).tolist()
+
+# Read in the dataframe of average GHI per set of coordinates 
 irr_avg_DF = sql.read.format('csv').load("/user/ubuntu/IrradianceMap/AverageIrradiance_df", inferSchema=True)
+
+# Rename to columns and sort in order of increasing average GHI
 irr_avg_DF = irr_avg_DF.withColumnRenamed("_c0", "Lat")
 irr_avg_DF = irr_avg_DF.withColumnRenamed("_c1", "Long")
 irr_avg_DF = irr_avg_DF.withColumnRenamed("_c2", "GHI_Avg")
-irr_avg_DF.show(n=10)
+irr_avg_DF = irr_avg_DF.sort("GHI_Avg")
+#irr_avg_DF.show(n=10)
 
-# Function to randomize the GHI for each coordinate by multiplying it by a random number between 0.9 and 1.1
-def randomize_GHI(GHI):
-    np.random.seed(0)
-    return np.random.uniform(0.9, 1.1) * GHI
-    
-# Make a new column with the average GHI multiplied by a random number between 0.9 and 1.1 
-randomize_GHI_udf = f.udf(randomize_GHI, FloatType())
-irr_avg_DF_sample = irr_avg_DF.withColumn("GHI_Avg_Rand", randomize_GHI_udf(f.col("GHI_Avg"))).sort("GHI_Avg_Rand", ascending=False).limit(100)
-#irr_avg_DF_sample.show(n=10)
+# Randomly select the indices for the solar site locations, with a preference for locations with higher GHI
+N_coords = irr_avg_DF.count()
+selection = sample_gaussian(100, N_coords)
+
+# Convert the dataframe to an RDD, add the index to each ntuple, and convert back to a dataframe
+# so that each row of the dataframe now has a row index
+irr_avg_RDD = irr_avg_DF.rdd.map(tuple)
+irr_avg_RDD = irr_avg_RDD.zipWithIndex().map(lambda x: (x[0][0], x[0][1], x[0][2], x[1]))
+irr_avg_DF = irr_avg_RDD.toDF(["Lat", "Long", "GHI_Avg", "Index"])
+
+# Filter for the selected indices
+irr_avg_DF_sample = irr_avg_DF.filter(irr_avg_DF.Index.isin(selection))
+#irr_avg_DF_sample.show(n=5)
 
 # Select the latitude and longitude columns to save
 irr_avg_DF_sample_coords = irr_avg_DF_sample.select("Lat", "Long")
@@ -73,7 +89,5 @@ plt.legend()
 plt.savefig("%s/Plots/IrradianceMap.png"%repo_path)
 
 ################################################################
-
-
 
 print("Time Elapsed: %ds"%(time.time()-start_time))
